55963f671f0821145b010045381c0ac8b4446058
thebigsmileXD <thebigsmilexd@gmail.com>
2018-09-15 01:31:21 +0200

    Change PluginTask to Task

--- src/xenialdan/gameapi/task/StartTickerTask.php
+++ src/xenialdan/gameapi/task/StartTickerTask.php
@@ -2,18 +2,13 @@
 
 namespace xenialdan\gameapi\task;
 
-use pocketmine\plugin\Plugin;
 use pocketmine\scheduler\PluginTask;
+use pocketmine\scheduler\Task;
 use xenialdan\gameapi\Arena;
 
-class StartTickerTask extends PluginTask{
+class StartTickerTask extends Task {
 
 	private $arena;
-
-	public function __construct(Plugin $plugin, Arena $arena){
-		parent::__construct($plugin);
-		$this->arena = $arena;
-	}
 
 	public function onRun(int $currentTick){
 		if ($this->arena instanceof Arena){
